e242a6a06e78a88bb6aff6054736376e4e0caf3f
Matthieu Jeanson <matthieu.jeanson@gmail.com>
2026-08-05 23:26:06 +0200

    refactor(debug): remove redundant node path lookup in debug label

--- debug_label.gd
+++ debug_label.gd
@@ -1,15 +1,12 @@
 extends Label
 
-# Temporary debug code to add to pyramid_scale.gd or a main script:
-func _process(_delta):
+func _process(_delta: float) -> void:
+	if not visible:
+		return
+
 	var grav = Input.get_gravity()
 	var accel = Input.get_accelerometer()
 	var gyro = Input.get_gyroscope()
-	
-	# Assuming you have a Label node named "Label" in your scene
-	if has_node("/root/Node3D/CanvasLayer/Label"): 
-		get_node("/root/Node3D/CanvasLayer/Label").text = (
-			"Gravity: " + str(grav) + "\n" +
-			"Accel: " + str(accel) + "\n" +
-			"Gyro: " + str(gyro)
-		)
+
+	text = "Gravity: %s\nAccel: %s\nGyro: %s" % [grav, accel, gyro]
+
